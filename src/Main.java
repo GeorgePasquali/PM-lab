0640a4ba41e3a5cc253043915e7d980479a7bc0c
Kali Na <Kali Na@PC1>
2018-02-13 10:46:11 +0200

    hihi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I am Very Nice :D ");
+        System.out.println("I am Very Nice: ) :D ");
     }
 }
